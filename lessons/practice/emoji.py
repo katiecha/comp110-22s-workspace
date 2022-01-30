b867634b60b5b727dcbce463bf5f685891229511
Katie Chai <kchai@unc.edu>
2022-01-30 04:07:27 -0500

    New lesson on characters

---
+++ lessons/practice/emoji.py
@@ -0,0 +1,15 @@
+print(chr(129312))
+
+print(ord("C") > ord("A"))
+
+print("C" > "A")
+
+print(chr(129313))
+
+print("110 is on a \U0001F6A2")
+
+print("Hello\nworld\n!!!")
+
+age: int = 21
+msg: str = f"You are {age}!"
+print(msg)
