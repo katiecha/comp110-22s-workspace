e80f071b7473143fea589778e723e439dadcb643
Katie Chai <kchai@unc.edu>
2022-02-03 18:47:53 -0500

    Exercise 03 Wordle with no comments

---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,75 @@
+"""EX03 - Wordle with fuctions!"""
+__author__ = "730460523"
+
+# Defining constants.
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+
+def main() -> None:
+    """The entrypoint o the program and main game loop."""
+    secret_word: str = "hi"
+    len_secret_word: int = len(secret_word)
+    user_guess: str = ""
+    emoji_hint: str = ""
+    turns: int = 1
+
+    while turns <= 6:
+        print(f"=== Turn {turns}/6 ===")
+        user_guess = input_guess(len_secret_word)
+        emoji_hint = emojified(user_guess, secret_word)
+        print(emoji_hint)
+
+        if user_guess == secret_word:
+            print(f"You won in {turns}/6 turns!")
+            turns = 7
+        elif user_guess != secret_word and turns < 6:
+            turns += 1
+        else:
+            turns += 1
+            print("X/6 - Sorry try again tomorrow!")
+
+
+def contains_char(word: str, char: str) -> bool:
+    """Determines if a word has a character in it."""
+    assert len(char) == 1
+    i: int = 0
+    word_len = int(len(word))
+
+    while i < word_len:
+        if word[i] == char:
+            return True
+        else:
+            i += 1
+    return False
+
+
+def emojified(guess: str, secret: str) -> str:
+    """Produces an emoji hint."""
+    assert len(guess) == len(secret)
+    hint: str = ""
+    count: int = 0
+    guess_len = int(len(guess))
+
+    while count < guess_len:
+        if guess[count] == secret[count]:
+            hint += GREEN_BOX
+        elif contains_char(secret, guess[count]):
+            hint += YELLOW_BOX
+        else:
+            hint += WHITE_BOX
+        count += 1        
+    return hint
+
+
+def input_guess(length: int) -> str:
+    """Collects input from user."""
+    iguess: str = (input(f"Enter a {length} character word: "))
+    while len(iguess) != length:
+        iguess = input(f"That wasn't {length} characters! Try again: ")
+    return(iguess)
+
+
+if __name__ == "__main__":
+    main()
